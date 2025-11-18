adf106151ace63df21ee82f3eb2845d4efb8b4f9
Andrew David Kay <andrewdavidkay@icloud.com>
2025-11-18 16:42:41 -0500

    initial uses page

---
+++ app/uses/page.tsx
@@ -0,0 +1,17 @@
+export default function Uses() {
+  return (
+    <div>
+      <h1>Uses</h1>
+
+      <p>
+        <ul>
+          <li>editor: Cursor</li>
+          <li>os: macOS</li>
+          <li>browser: Brave</li>
+          <li>terminal: Ghostty</li>
+          <li>shell: zsh</li>
+        </ul>
+      </p>
+    </div>
+  )
+}
